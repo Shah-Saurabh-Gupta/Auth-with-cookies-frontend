fff4ddc3ee5e0ae5bef5e5aa65f0cfa93b7f5214
Saurabh Gupta <saurabhg@uvxcel.com>
2021-10-16 11:50:04 +0530

    Navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from "react-router-dom"
+import AuthContext from '../../context/AuthContext'
+import LogOutBtn from '../Auth/LogOutBtn'
 
 function Navbar() {
+
+    const { loggedIn } = useContext(AuthContext)
+
     return (
         <div>
             <Link to="/">Home</Link>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/customer">Customer</Link>
+            {loggedIn === false && (
+                <>
+                    <Link to="/register">Register</Link>
+                    <Link to="/login">Login</Link>
+                </>
+            )}
+            {loggedIn === true && (
+                <>
+                    <Link to="/customer">Customer</Link>
+                    <LogOutBtn />
+                </>
+            )}
         </div>
     )
 }
