98e9ae847ef5b13d802fe8bad591904f2f3354b7
Saurabh Gupta <saurabhg@uvxcel.com>
2021-10-16 11:48:39 +0530

    App and Routes

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import Customers from './components/customers/Customers';
 import Navbar from './components/layouts/Navbar';
+import AuthContext from './context/AuthContext';
 
 function Router() {
+
+    const { loggedIn } = useContext(AuthContext);
+
     return <BrowserRouter>
         <Navbar />
         <Switch>
             <Route exact path="/">
                 <div>Home</div>
             </Route>
-            <Route path="/register">
-                <Register />
-            </Route>
-            <Route path="/login">
-                <Login />
-            </Route>
-            <Route path="/customer">
-                <div>Customers</div>
-            </Route>
+            {loggedIn === false && (
+                <>
+                    <Route path="/register">
+                        <Register />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                </>
+            )}
+            {loggedIn === true && (
+                <>
+                    <Route path="/customer">
+                        <Customers />
+                    </Route>
+                </>
+            )}
         </Switch>
     </BrowserRouter>
 }
